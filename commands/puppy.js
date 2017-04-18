2ecaf0eeec97d6c525145a491939c0eca156442f
Darqam <anhim2@gmail.com>
2017-04-18 16:02:56 -0600

    Update puppy.js
    Made the bot check if image was dead or not, if so get a new one.

--- commands/puppy.js
+++ commands/puppy.js
@@ -1,11 +1,30 @@
 const randomPuppy = require('random-puppy');
+const snekfetch = require('snekfetch');
+
+function newPic(url, cb){
+    snekfetch.get(url).then( r => {
+      if(r.url !== 'http://i.imgur.com/removed.png') return cb(url);
+      console.log('Grabbing new puppy link due to dead image');
+      randomPuppy()
+        .then(pupUrl => {
+          newPic(pupUrl, function(callB){//recursively call this until the image is good
+            return cb(callB);
+          });
+        });
+    });
+}
 
 exports.run = (client, message) => {
 
 randomPuppy()
-    .then(url => {
-        message.channel.sendMessage(url);
+  .then(url => {
+    message.channel.sendMessage(url).then(msg =>{
+      newPic(url, function afterUrl(newUrl){
+        if(newUrl === url) return;
+        return msg.edit(newUrl);
+      });
     });
+  });
 };
 
 exports.conf = {
@@ -18,6 +37,6 @@ exports.conf = {
 
 exports.help = {
   name: 'puppy',
-  description: 'Links a random cat picture',
+  description: 'Links a random puppy picture',
   usage: 'puppy'
 };
